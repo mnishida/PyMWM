9a683ff8ed578a8cd54dab28c43141654c454289
mnishida <mnishida@hiroshima-u.ac.jp>
2016-11-30 21:38:09 +0900

    test_Yaa_and_Y is added.

--- pymwm/slit/test/test_slit.py
+++ pymwm/slit/test/test_slit.py
@@ -125,3 +125,28 @@ def test_Yab_with_loss():
     npt.assert_almost_equal(
         wg.Yab(w, h1, s1, l1, n1, m1, a1, b1,
                h2, s2, l2, n2, m2, a2, b2), 0.0, decimal=10)
+
+
+def test_Yaa_and_Y():
+    import numpy as np
+    import numpy.testing as npt
+    import pymwm
+    params = {'core': {'shape': 'slit', 'size': 0.15,
+                       'fill': {'model': 'air'}},
+              'clad': {'model': 'gold_dl', 'im_factor': 1.0},
+              'bounds': {'lmax': 3.0, 'lmin': 0.575, 'limag': 10.0},
+              'modes': {'num_n': 6}}
+    wg = pymwm.create(params)
+    w = 2 * np.pi / 0.575
+    for i, alpha in enumerate(wg.alpha_all):
+        h = wg.beta(w, alpha)
+        a, b = wg.coef(h, w, alpha)
+        s = wg.s_all[i]
+        l = wg.l_all[i]
+        n = wg.n_all[i]
+        m = wg.m_all[i]
+        Yab = wg.Yab(w, h, s, l, n, m, a, b, h, s, l, n, m, a, b)
+        Y = wg.Y(w, h, alpha, a, b)
+        print(Yab)
+        print(Y)
+        npt.assert_almost_equal(Yab, Y)
