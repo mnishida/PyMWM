f20f60fa79f68750ed83ad362a863668ac2a9663
mnishida <mnishida@hiroshima-u.ac.jp>
2017-06-23 19:06:14 +0900

    Set mkl path in setup.py

--- setup.py
+++ setup.py
@@ -3,6 +3,7 @@ from setuptools import setup
 from numpy.distutils.core import Extension
 from distutils.util import get_platform
 from Cython.Build import cythonize
+import os
 from os import path
 import subprocess
 import numpy as np
@@ -12,9 +13,9 @@ long_description = """
 PyMWM is a metallic waveguide mode solver witten in Python.
 """
 
-blas_params = np.__config__.blas_opt_info
-blas_library_dirs = blas_params['library_dirs']
-blas_libraries = blas_params['libraries']
+# blas_params = np.__config__.blas_opt_info
+# blas_library_dirs = blas_params['library_dirs']
+# blas_libraries = blas_params['libraries']
 # lapack_params = np.__config__.lapack_opt_info
 # lapack_library_dirs = lapack_params['library_dirs']
 # lapack_libraries = lapack_params['libraries']
@@ -32,7 +33,18 @@ cylinder_src = [
 platform = get_platform()
 if platform.startswith('win'):
     extra_compile_args = []
-    extra_link_args = []
+    extra_link_args = [
+        path.join(complex_bessel, 'src', 'amos_iso_c_fortran_wrapper.o'),
+        path.join(complex_bessel, 'src', 'machine.o'),
+        path.join(complex_bessel, 'src', 'zbesh.o')
+    ]
+    mkl_include = os.path.abspath(os.path.join(
+        os.sep, 'Program Files (x86)', 'IntelSWTools',
+        'compilers_and_libraries', 'windows', 'mkl', 'include'))
+    mkl_library_dirs = [os.path.abspath(os.path.join(
+        os.sep, 'Program Files (x86)', 'IntelSWTools',
+        'compilers_and_libraries', 'windows', 'mkl', 'lib', 'intel64'))]
+    mkl_libraries = ['mkl_rt']
     cmd = "gfortran -c {0}.f90 -o {0}.o -m64 -march=native -O3".format(
         path.join(complex_bessel, 'src', 'amos_iso_c_fortran_wrapper'))
     subprocess.call(cmd, shell=True)
@@ -45,7 +57,17 @@ if platform.startswith('win'):
 else:
     extra_compile_args = ['-fPIC', '-m64', '-fopenmp', '-march=native', '-O3',
                           '-ftree-vectorizer-verbose=2', '-Wl,--no-as-needed']
-    extra_link_args = ['-shared']
+    extra_link_args = [
+        '-shared',
+        path.join(complex_bessel, 'src', 'amos_iso_c_fortran_wrapper.o'),
+        path.join(complex_bessel, 'src', 'machine.o'),
+        path.join(complex_bessel, 'src', 'zbesh.o')
+    ]
+    mkl_include = os.path.abspath(os.path.join(
+        os.sep, 'opt', 'intel', 'mkl', 'include'))
+    mkl_library_dirs = [os.path.abspath(os.path.join(
+        os.sep, 'opt', 'intel', 'mkl', 'lib', 'intel64'))]
+    mkl_libraries = ['mkl_rt', 'gfortran', 'dl', 'm']
     cmd = "gfortran -c {0}.f90 -o {0}.o -fPIC -m64 -march=native -O3".format(
         path.join(complex_bessel, 'src', 'amos_iso_c_fortran_wrapper'))
     subprocess.call(cmd, shell=True)
@@ -55,22 +77,16 @@ else:
     cmd = "gfortran -c {0}.for -o {0}.o -fPIC -m64 -march=native -O3".format(
         path.join(complex_bessel, 'src', 'zbesh'))
     subprocess.call(cmd, shell=True)
-extra_link_args = [
-    '-shared',
-    path.join(complex_bessel, 'src', 'amos_iso_c_fortran_wrapper.o'),
-    path.join(complex_bessel, 'src', 'machine.o'),
-    path.join(complex_bessel, 'src', 'zbesh.o')
-]
 slit_pyx = path.join('pymwm', 'slit', 'utils', 'slit_utils.pyx')
 # library_dirs = blas_library_dirs + lapack_library_dirs
 # libraries = blas_libraries + lapack_libraries + ['gfortran', 'dl', 'm']
-library_dirs = blas_library_dirs
-libraries = blas_libraries + ['gfortran', 'dl', 'm']
+library_dirs = mkl_library_dirs
+libraries = mkl_libraries + ['gfortran', 'dl', 'm']
 extentions = [
     Extension("pymwm.cylinder.utils.cylinder_utils",
               sources=cylinder_src,
               depends=[c_complex_bessel_h],
-              include_dirs=[np.get_include(), ".",
+              include_dirs=[np.get_include(), ".", mkl_include,
                             path.join(complex_bessel, 'include')],
               extra_compile_args=extra_compile_args,
               extra_link_args=extra_link_args,
@@ -80,7 +96,7 @@ extentions = [
     Extension("pymwm.slit.utils.slit_utils",
               sources=[slit_pyx],
               depends=[],
-              include_dirs=[np.get_include(), "."],
+              include_dirs=[np.get_include(), ".", mkl_include],
               extra_compile_args=extra_compile_args,
               extra_link_args=['-shared'],
               library_dirs=library_dirs,
