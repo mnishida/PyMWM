a7afc62dda1f62ed567cf80fbb45d5e096c1668f
mnishida <mnishida@hiroshima-u.ac.jp>
2021-08-18 15:02:37 +0900

    Bump version: 0.1.1 → 0.1.2

--- docs/conf.py
+++ docs/conf.py
@@ -1,7 +1,7 @@
 from recommonmark.transform import AutoStructify
 
 project = "pymwm"
-version = "0.1.1"
+version = "0.1.2"
 copyright = "2021, Munehiro Nishida"
 author = "Munehiro Nishida"
 
--- pymwm/__init__.py
+++ pymwm/__init__.py
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-
 
 __author__ = "Munehiro Nishida"
-__version__ = "0.1.1"
+__version__ = "0.1.2"
 __license__ = "MIT"
 
 
