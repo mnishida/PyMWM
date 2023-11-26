508dab7d9868785f088cafc064445a806a3d0d00
mnishida <mnishida@hiroshima-u.ac.jp>
2023-11-26 17:40:57 +0900

    v0.5.4

--- docs/conf.py
+++ docs/conf.py
@@ -1,7 +1,7 @@
 from recommonmark.transform import AutoStructify
 
 project = "PyMWM"
-version = "0.5.3"
+version = "0.5.4"
 copyright = "2021, Munehiro Nishida"
 author = "Munehiro Nishida"
 
--- src/pymwm/__init__.py
+++ src/pymwm/__init__.py
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-
 
 __author__ = "Munehiro Nishida"
-__version__ = "0.5.3"
+__version__ = "0.5.4"
 __license__ = "MIT"
 
 
